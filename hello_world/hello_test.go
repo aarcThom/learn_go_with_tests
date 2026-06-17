195b465eeed709033bc945fa28c3d329f8c95702
aarcThom <robertthomasgaudin@gmail.com>
2026-06-16 23:22:26 -0700

    Continue working through hello world

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Thomas")
+	want := "Hello, Thomas"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
